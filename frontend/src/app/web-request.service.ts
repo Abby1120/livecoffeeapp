61506b4d8260e927456c0ebb9622acf06b61d192
Abigail Gulla <abigailgulla@gmail.com>
2020-03-14 00:59:18 -0400

    index

--- frontend/src/app/web-request.service.ts
+++ frontend/src/app/web-request.service.ts
@@ -9,7 +9,7 @@ export class WebRequestService {
   readonly ROOT_URL;
 
   constructor(private http: HttpClient) {
-    this.ROOT_URL = 'https://coffeetrial.herokuapp.com';
+    this.ROOT_URL = '';
    }
 
   get(uri: string) {
