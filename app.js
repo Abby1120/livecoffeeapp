b0ffc358e9fe5b3baea579d695e5a05974bba275
Abigail Gulla <abigailgulla@gmail.com>
2020-03-14 15:26:16 -0400

    underscore

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const jwt = require('jsonwebtoken');
 // Load middleware
 app.use(bodyParser.json());
 
-app.use(express.static(_dirname + '/dist/frontend'));
+app.use(express.static(__dirname + '/dist/frontend'));
 
 app.get('*', function(req,res) {
     res.sendFile(path.join(__dirname + '/dist/frontend/index.html'));
