6641ef81c2070b71f91ec0c0f40f564b72703b88
Abigail Gulla <abigailgulla@gmail.com>
2020-03-14 23:44:34 -0400

    this is crazy

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static('frontend/dist'));
 //   });
   
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/dist/frontend/index.html'));
   });
 
 // app.get('*', (req, res) => {
